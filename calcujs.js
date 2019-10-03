02ad397e64f40647f17cc93789bb1f261c8831c0
qadriyyahm <55480192+qadriyyahm@users.noreply.github.com>
2019-10-03 11:38:00 +0800

    Update calcujs.js

--- calcujs.js
+++ calcujs.js
@@ -1,81 +1,282 @@
-function maxi(){
-    var val1 = document.formnya.nilai1.value;
-    var val2 =document.formnya.nilai2.value;
-    var val3 =document.formnya.nilai3.value;
-    var max =Math.max(val1,val2,val3);
-    //document.getElementById('maxi').value = max;
-    return max; 
+@import url(http://fonts.googleapis.com/css?family=Open+Sans:400,800,700,600,300);
+ 
+body {
+	background: #FFE4E1;
+	margin:0;
+	font-family: 'Century Gothic', sans-serif;
+    font-size : 1.5em
+}
+ 
+hr {
+	border:0;
+	background:#dedede;
+	height:1px;
+}		
+ 
+header{
+	text-align: center;
+	color: #232323;
+}
+ 
+header .judul{
+	font-size: 35pt;
+}
+ 
+header .deskripsi{
+	font-size: 11pt;
+}
+ 
+.wrap {
+	width:650px;
+  margin:0 auto;
+}
+ 
+nav.menu ul {
+	overflow:hidden;
+	color:#fff;
+	list-style: none;
+	float:left;
+	padding:0;
+	width: 650px;
+	margin:0 0 0;
+	text-align : center;
+	background: #FA8072;
+	-webkit-box-shadow: 1px 1px 1px 0px rgba(204,204,204,0.55);
+	-moz-box-shadow: 1px 1px 1px 0px rgba(204,204,204,0.55);
+	box-shadow: 1px 1px 1px 0px rgba(204,204,204,0.55);
+}
+ 
+nav.menu ul li {
+	margin:0;
+	float:center; 
+	display: inline-block;
+}
+ 
+nav.menu ul a {
+	padding:25px;
+	display:block;
+	font-weight:600;
+	font-size: 16px;
+	color:#fff;
+	text-transform: uppercase;
+	transition: all 0.5s ease;
+	text-decoration: none;
+}
+ 
+nav.menu ul a:hover {
+	text-decoration: underline;
+	background:#16a085;
+}
+ 
+.sidebar {
+	float:right;
+	width:275px;
+}
+ 
+.sidebar .widget {
+	padding:25px;
+	margin:0 0 25px;
+	background:#fff;
+	border-bottom: 2px solid #fff;
+	transition: all 0.5s ease;
+}
+ 
+.sidebar .widget:hover {
+	border-bottom: 2px solid #3498db;
+}
+ 
+.sidebar .widget h2 {
+	padding:0;
+	margin:0 0 15px;
+	color:#3498db;
+	font-size: 18px;
+	font-weight:800;
+	text-transform: uppercase;
+}
+ 
+.sidebar .widget p {
+	font-size: 14px;
+}
+ 
+.sidebar .widget p:last-child {
+	margin:0;
+}
+ 
+.home {
+	text-align : center;
 }
 
-function mini(){
-    var val1 = document.formnya.nilai1.value;
-    var val2 =document.formnya.nilai2.value;
-    var val3 =document.formnya.nilai3.value;
-    var mini =Math.min(val1,val2,val3);
-    //document.getElementById('mini').value = max;
-    return mini; 
+.jelaskan {
+	font-size : 8pt;
+	text-align : center;
 }
 
-function meann(){
-    var val1 = document.formnya.nilai1.value;
-    var val2 =document.formnya.nilai2.value;
-    var val3 =document.formnya.nilai3.value;
-    var mean =(parseInt(val1)+parseInt(val2)+parseInt(val3))/3;
-    //document.getElementById('meann').value = mean;
-    return mean; 
+.sekolah {
+	font-size : 12px;
 }
 
-function mediann(){
-    var val1 = document.formnya.nilai1.value;
-    var val2 =document.formnya.nilai2.value;
-    var val3 =document.formnya.nilai3.value;
-    var jum  = new Array(val1,val2,val3).sort();
-    //document.getElementById('mediann').value = median;
-    return jum[1]; 
+ 
+.blog {
+	
+	float:left;
 }
-function bersihkan() {
-    var val1 = document.formnya.nilai1.value = "";
-    var val2 =document.formnya.nilai2.value = "";
-    var val3 =document.formnya.nilai3.value = "";
-    var hasil = document.getElementById("hasilHitung");
-    hasil.innerHTML = "";
+
+.profile
+  {
+    background : white;
+    font-size : 0.5em;
+  }
+
+.profile img{
+	float:right;
 }
-function modus() {
-  var val1 = document.formnya.nilai1.value;
-  var val2 =document.formnya.nilai2.value;
-  var val3 =document.formnya.nilai3.value;
-    let data = [val1, val2, val3];
-    let hitung = 0, max_hitung = 0, modus = 0;
-    for (let i = 0; i < data.length; i++) {
-        hitung = 0;
-        for (let j = 0; j < data.length; j++) {
-            if(data[i] == data[j]) {
-                hitung++;
-            }
-        }
-        if(hitung > max_hitung) {
-            max_hitung = hitung;
-            modus = data[i];
-        }
-    }
-    if(max_hitung > 1) {
-        return modus;
-    }
-    else {
-        return "Tidak ada modus";
-    }
-    
+	
+  .sansserif {
+	background : #FFDAB9;
+	font-family: Century Gothic;
+	height : 300px;
+	width : 400px;
+  }
+
+td{padding-left:5px;}
+
+.conteudo {
+	width:600px;
+	padding:25px;
+	margin:25px auto;
+	background: #fff;
+	border:1px solid #dedede;
+	-webkit-box-shadow: 1px 1px 1px 0px rgba(204,204,204,0.35);
+	-moz-box-shadow: 1px 1px 1px 0px rgba(204,204,204,0.35);
+	box-shadow: 1px 1px 1px 0px rgba(204,204,204,0.35);
 }
-function hasil() {
-  var val1 = document.formnya.nilai1.value;
-  var val2 =document.formnya.nilai2.value;
-  var val3 =document.formnya.nilai3.value;
-    var hasil = document.getElementById("hasilHitung");
-    hasil.innerHTML = "<table>\
-    <tr><td>Mean</td><td>:</td><td>" + meann(val1,val2,val3) + " </td></tr> \
-    <tr><td>Median</td><td>:</td><td>" + mediann(val1,val2,val3) + "</td></tr> \
-    <tr><td>Modus</td><td>:</td><td>" + modus(val1,val2,val3) + "</td></tr> \
-    <tr><td>Max</td><td>:</td><td>" + maxi(val1,val2,val3) + "</td></tr> \
-    <tr><td>Min</td><td>:</td><td>" + mini(val1,val2,val3) + "</td></tr> \
-    </table>"
+ 
+.conteudo img{
+	 width: 32%;
+}
+ 
+.conteudo h1 {
+	padding:0;
+	margin:0 0 15px;
+	font-weight: normal;
+	color: #666;
+	font-family: Georgia;
+}
+ 
+.conteudo p:last-child {
+	margin: 0;
+}
+ 
+.conteudo .continue-lendo {
+	color:#000;
+	transition: all 0.5s ease;
+	text-decoration: none;
+	font-weight: 700; 
+}
+
+table{
+	border:0px solid transparent;
+} 
+
+.conteudo .continue-lendo:hover {
+	margin-left:10px;
+}
+ 
+.post-info {
+	float: right;
+	font-size: 12px;
+	margin: -10px 0 15px;
+	text-transform: uppercase;
+}
+ 
+@media screen and (max-width: 960px) {
+ 
+	.header {
+		position:inherit;
+	}
+ 
+	.wrap {
+		width: 90%;
+		margin:25px auto;
+	}
+	.sidebar {
+		width:100%;
+		margin:25px 0 0;
+		float:right;
+	}
+ 
+	.sidebar .widget {
+		padding:5%;
+	}
+ 
+	nav.menu ul {
+		width: 100%;
+	}
+ 
+	nav.menu ul {
+		float:inherit;
+	}
+ 
+	nav.menu ul li {
+		float:inherit;
+		margin:0;
+	}
+ 
+	nav.menu ul a {
+		padding:15px;
+		font-size: 16px;
+		border-top:1px solid #1abf9f;
+		border-bottom:1px solid #16a085;
+	}
+ 
+	.blog {
+		width:90%;
+	}
+ 
+	.conteudo {
+		float:inherit;
+		margin:0 auto 25px;
+		width:101%;
+		border:1px solid #dedede;
+		padding:5%;  
+		background: #fff;
+	}
+ 
+	.conteudo img {
+		max-width: 110%;
+		margin:0 0 25px -5%;
+		min-width: 110%;
+	}
+ 
+	.conteudo .continue-lendo:hover {
+		margin-left:0;
+	}
+	
+ 
+@media screen and (max-width: 460px) {
+ 
+	nav.menu ul a {
+		padding:15px;
+		font-size: 14px;
+	}
+ 
+	.sidebar {
+		display:none
+	}
+	.post-info {
+		display:none;
+	}
+ 
+	.atur {
+		text-align : justify;
+		font-size : 12pt;
+	}
+ 
+	.conteudo {
+		margin:25px auto;
+	}
+ 
+	.conteudo img {
+		margin:-5% 0 25px -5%;
+	}			
 }
